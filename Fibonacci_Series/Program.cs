a91449bc47c14bc65c9073e24a6fa423071e039b
Yuvraj <yuvrajtarate47@gmail.com>
2023-05-06 13:11:47 +0530

    adding fibonacci progaram code

--- Fibonacci_Series/Program.cs
+++ Fibonacci_Series/Program.cs
@@ -4,9 +4,28 @@ namespace Fibonacci_Series
 {
     class Program
     {
+      
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int prevnum = 0;
+            int nextNum = 1;
+            int sum = 0;
+            int num = 13;
+            for (int i = 0; i < num; i++)
+            {
+                sum = prevnum + nextNum;
+                if (sum >= 100)
+                {
+                    break;
+                }
+                Console.WriteLine(sum);
+                prevnum = nextNum;
+                nextNum = sum;
+                
+                
+            }
+            
         }
     }
 }
